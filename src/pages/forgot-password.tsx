15ba6240caa6d2a110ccd3a8b5b924f05442ed31
KhusniddinovUz <khusniddinovuz@gmail.com>
2023-07-19 21:33:51 +0500

    Create forgot password component

---
+++ src/pages/forgot-password.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ForgotPasswordSection from "@/section/forgot-password";
+
+const ForgotPassword = () => {
+  return(
+      <ForgotPasswordSection/>
+  )
+};
+
+export default ForgotPassword;
---
+++ src/section/forgot-password/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {TitleWrapper} from "@/section/login/login.style";
+import Typography from "@/components/typography";
+import Input from "@/components/input";
+import EnvelopeIcon from "@/components/icons/EnvelopeIcon";
+import Button from "@/components/button";
+import BaseLayout from "@/layouts/base-layout";
+
+const ForgotPasswordSection = () => {
+    return(
+        <BaseLayout>
+            <TitleWrapper>
+                <Typography $align={"center"} $textSize="title" $textWeight={"w_700"} color={"black"}>
+                    Forgot your password?
+                </Typography>
+                <Typography $align={"center"} $textSize={"subTitle"} $textWeight={"w_400"} color={"text"}>
+                    Enter your email and get OTP to verification.
+                </Typography>
+            </TitleWrapper>
+            <Input icon={<EnvelopeIcon/>} placeholder="Email" $marginTop={"25px"}/>
+            <Input icon={<EnvelopeIcon/>} placeholder="Repeat Email" $marginTop={"10px"}/>
+            <Button color="white" $backgroundcolor="primary">
+                GET OTP
+            </Button>
+        </BaseLayout>
+    )
+};
+
+export default ForgotPasswordSection;
