f9c742e97edf1cee0bfc4fb3d167c26c57168557
KhusniddinovUz <khusniddinovuz@gmail.com>
2023-07-19 21:39:44 +0500

    Create new password page

---
+++ src/pages/new-password.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NewPasswordSection from "@/section/new-password";
+
+const NewPassword = ()=> {
+    return(
+        <NewPasswordSection/>
+    )
+};
+
+export default NewPassword;
---
+++ src/section/new-password/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {TitleWrapper} from "@/section/login/login.style";
+import Typography from "@/components/typography";
+import Input from "@/components/input";
+import Button from "@/components/button";
+import BaseLayout from "@/layouts/base-layout";
+import ShieldIcon from "@/components/icons/ShieldIcon";
+
+const NewPasswordSection = () => {
+    return(
+        <BaseLayout>
+            <TitleWrapper>
+                <Typography $align={"center"} $textSize="title" $textWeight={"w_700"} color={"black"}>
+                    Enter your new password
+                </Typography>
+                <Typography $align={"center"} $textSize={"subTitle"} $textWeight={"w_400"} color={"text"}>
+                    This is the last step in recovering your password.
+                </Typography>
+            </TitleWrapper>
+            <Input icon={<ShieldIcon/>} placeholder="Password" $marginTop={"25px"}/>
+            <Input icon={<ShieldIcon/>} placeholder="Confirm Password" $marginTop={"10px"}/>
+            <Button color="white" $backgroundcolor="primary">
+                SUBMIT
+            </Button>
+        </BaseLayout>
+    )
+};
+
+export default NewPasswordSection;
