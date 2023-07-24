d256cabf140678f626cfbc187cf58be51afbeb95
KhusniddinovUz <khusniddinovuz@gmail.com>
2023-07-24 15:53:36 +0500

    Add text to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>WELCOME HOME!</h1>
+    </div>
+  );
 }
